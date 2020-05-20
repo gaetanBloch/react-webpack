708bb77372ead6c1f539fa5338957526987c707a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-20 15:11:47 +0200

    Set up Links in the App Container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render () {
     return (
+      <div>
+        <div>
+          <Link to="/" exact >Users</Link> |
+          <Link to="/pizza">Pizza</Link>
+        </div>
+      </div>
     );
   }
 }

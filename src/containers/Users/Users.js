450fa25560136836a891331337aa3446393238e0
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-20 15:08:14 +0200

    Added Content to the Users Container

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 class Users extends Component {
   render () {
     return (
+      <div>
+        <h1>The Users</h1>
+        <p>Awesome user on board!</p>
+      </div>
     );
   }
 }
